d3e04a1cc96af175376b5d2799cc9d6c59f0ba2f
Glenthedeveloper <hanongibs@gmail.com>
2023-03-26 16:43:35 -0700

    handlerfile

--- write_handlers.c
+++ write_handlers.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /************************* WRITE HANDLE *************************/
 /**
@@ -250,3 +250,4 @@ int write_pointer(char buffer[], int ind, int length,
 		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], BUFF_SIZE - ind - 1));
 }
+
